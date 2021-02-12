677075ba8681ed46fe97b7fd015ebaf11e32b2a2
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2021-02-12 15:56:22 +0530

    main function

--- src/main/java/com/spring/loginexample/LoginexampleApplication.java
+++ src/main/java/com/spring/loginexample/LoginexampleApplication.java
@@ -3,6 +3,7 @@ package com.spring.loginexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main driver class
 @SpringBootApplication
 public class LoginexampleApplication {
 
