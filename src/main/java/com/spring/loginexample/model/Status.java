11337a3cb904e19eea05be53a31f16750a5bc293
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2021-02-12 15:40:59 +0530

    response status

--- src/main/java/com/spring/loginexample/model/Status.java
+++ src/main/java/com/spring/loginexample/model/Status.java
@@ -1,5 +1,10 @@
 package com.spring.loginexample.model;
 
+//return response to user when a request is sent
+//SUCCESS states that the request sent is successfully processed
+//ALREADY_EXISTS : if a user sign ups with same username or email this response will be returned
+//FAILURE
+
 public enum Status {
     SUCCESS,
     ALREADY_EXISTS,
