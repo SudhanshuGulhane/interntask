96feb1babdd79e816318b81090098d2931977d42
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2021-02-12 15:43:35 +0530

    adding JpaRepository

--- src/main/java/com/spring/loginexample/repo/UserRepo.java
+++ src/main/java/com/spring/loginexample/repo/UserRepo.java
@@ -4,6 +4,9 @@ import com.spring.loginexample.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//extending JpaRepo for accessing the defined functions
+//for performing crud operations on request received from user
+
 @Repository
 public interface UserRepo extends JpaRepository<User,Integer> {
 }
